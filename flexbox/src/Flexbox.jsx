08a7f5feaa43f56cfb54243c335a3258795c5846
iamjosuacharles <josuacharles.n@gmail.com>
2024-04-29 13:57:58 +0100

    working on flexbox

---
+++ flexbox/src/Flexbox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ".Flexbox.css"
+
+function Flexbox() {
+    return (
+        <div className="container_flex" >
+            <div>This is my first function</div>
+            <div>Tobilola</div>
+            <div>Tomilola which has in </div>
+        </div>
+    )
+
+}
+
+export default Flexbox
